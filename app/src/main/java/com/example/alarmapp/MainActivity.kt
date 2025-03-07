be63ca68c6f564d751af3971b2cde577595d7a23
kuhre2 <151893679+kuhre2@users.noreply.github.com>
2025-03-07 12:18:16 +0700

    Add Exact Schedule Alarm permission

--- app/src/main/java/com/example/alarmapp/MainActivity.kt
+++ app/src/main/java/com/example/alarmapp/MainActivity.kt
@@ -5,10 +5,12 @@ import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -35,6 +37,7 @@ import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
 
+        @RequiresApi(Build.VERSION_CODES.S)
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContent {
@@ -43,6 +46,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     @Composable
     @Preview(showBackground = true)
     fun AlarmApp() {
@@ -108,27 +112,35 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     fun setAlarm(context: Context, calendar: Calendar, repeat: Boolean) {
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, AlarmReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
             context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
-
-        if (repeat) {
-            alarmManager.setRepeating(
-                AlarmManager.RTC_WAKEUP,
-                calendar.timeInMillis,
-                AlarmManager.INTERVAL_DAY,
-                pendingIntent
-            )
-        } else {
-            alarmManager.setExact(
-                AlarmManager.RTC_WAKEUP,
-                calendar.timeInMillis,
-                pendingIntent
-            )
+        try {
+            if(alarmManager.canScheduleExactAlarms()){
+                if (repeat) {
+                    alarmManager.setRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    AlarmManager.INTERVAL_DAY,
+                    pendingIntent
+                    )
+                } else {
+                    alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    pendingIntent
+                    )
+                }
+            } else {
+                Toast.makeText(context, "Ứng dụng không được phép đặt alarm", Toast.LENGTH_SHORT)
+            }
+        }catch(e: SecurityException) {
+            e.printStackTrace()
+            Toast.makeText(context, "Không thể đặt alarm do thiếu quyền", Toast.LENGTH_SHORT)
         }
-
         Toast.makeText(context, "Báo thức đã đặt!", Toast.LENGTH_SHORT).show()
     }
